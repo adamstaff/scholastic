22242a890a997c407a3a6cb2f2c561a43120cca5
spaffinge <52344911+spaffinge@users.noreply.github.com>
2023-07-24 18:03:10 +0100

    Update maidenbackup.lua

--- maidenbackup.lua
+++ maidenbackup.lua
@@ -1,3 +1,5 @@
+util = require "util"
+
 function redraw_clock() ----- a clock that draws space
   while true do ------------- "while true do" means "do this forever"
     clock.sleep(1/15) ------- pause for a fifteenth of a second (aka 15fps)
@@ -12,14 +14,18 @@ function init()
   redraw_clock_id = clock.run(redraw_clock) --add these for other clocks so we can kill them at the end
 
   currentTrack = 1
-  curXpos=0
-  curXwidth=0
+  curXpos=1
+  curXbeat=1
+  curXdiv=1
+  curXwidth=1
+  curXdisp = 1
+  maxXpos=1
+  displayWidthBeat=1
 
   rhythmicDisplay = {
-    {5, 3, 2, 4},
-    {4, 3},
-    {2, 2},
-    {1, 1, 1, 1, 1, 6}
+    {2, 5, 4, 1},
+    {4},
+    {1, 1, 1, 3}
   }
   redraw()
 end
@@ -49,10 +55,12 @@ function redraw()
   end
   
   -- rectangle for cursor
-  screen.level(2)
+  screen.level(1)
   
+  --calc y pos
   curYPos = (currentTrack - 1) * (64 / #rhythmicDisplay)
-  screen.rect(0, 0, 10, (64 / #rhythmicDisplay))
+  
+  screen.rect(curXdisp, curYPos, curXwidth, (64 / #rhythmicDisplay))
   screen.fill()
   
   screen.update()
@@ -62,21 +70,39 @@ function enc(e, d)
   --move cursor between tracks
   if (e == 1) then
     currentTrack = util.clamp(currentTrack + d, 1, #rhythmicDisplay)
-    curXpos = 0
-    for i=1, #rhythmicDisplay do
-    
+    curXpos = 1
+    -- calculate the cursor width
+    maxXpos = 0
+    for i=1, #rhythmicDisplay[currentTrack] do
+      maxXpos = maxXpos + rhythmicDisplay[currentTrack][i]
     end
-    maxXpos = totalBeats
+    print('maxXpos is ' .. maxXpos)
+    --calculate the width of the cursor
+    for i=1, #rhythmicDisplay[currentTrack] do
+      if curXpos < rhythmicDisplay[currentTrack][i] then
+        curXwidth = math.floor( 128 / (#rhythmicDisplay[currentTrack] * rhythmicDisplay[currentTrack][i]))
+        print('curXwidth is ' .. curXwidth)
+        break
+      end
+    end
     screenDirty = true
   end
 
   -- move cursor in time
   if (e == 2) then
-    curXpos = curXpos + d
-    displayWidthBeat = math.floor( 128 / #rhythmicDisplay[currentTrack] )
-    displayWidthSubdiv = math.floor(displayWidthBeat / rhythmicDisplay[i][j])
-    nowPosition = displayWidthBeat * (j - 1) + displayWidthSubdiv * (k - 1)
-    curXwidth = dispayWidthBeat
+    --in/decrement the position in the array
+    curXdiv = curXpos + d
+    if curXdiv > rhythmicDisplay[currentTrack][curXbeat] then
+      curXbeat = curXbeat+1
+      curXdiv = 1
+    end
+    if curXdiv < 1 then curXbeat = curXbeat-1 end
+    if curXbeat < 1 then curXbeat = 1 end
+    if curXbeat > #rhythmicDisplay[currentTrack] then curXbeat = #rhythmicDisplay[currentTrack] end
+    -- calculate the x position: beat + subdivision
+    -- offset by beat
+    curXdisp = (128 / curXbeat)
+    
     screenDirty = true
   end
 
