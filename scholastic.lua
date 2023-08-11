f0b485fa2e7a65c2586c81125ab19635ed8c9354
spaffinge <52344911+spaffinge@users.noreply.github.com>
2023-08-11 15:01:03 +0100

    1.0
    Completed drawing improvements
    Completed MIDI implemetation
    Added param for clock divide to make it easier to make long patterns (although perhaps not on the eyes)

--- scholastic.lua
+++ scholastic.lua
@@ -47,33 +47,48 @@ end
 --tick along, play events
 function ticker()
   while isPlaying do
-    if (clockPosition >= 4) then clockPosition = 0 end  --loop clock
     for i = 1, #noteEvents do                           -- play notes
       if noteEvents[i][3] and noteEvents[i][1] <= tracksAmount then
-        if math.floor(clockPosition*192) == math.floor(util.round(noteEvents[i][2] * 192 * 4, 0.0001)) then
+        if math.floor(util.round(clockPosition*192,0.0001)) == math.floor(util.round(noteEvents[i][2] * 192 * (4 * clock_div), 0.0001)) then
           local track = noteEvents[i][1]
 					-- if we want to play a note:
 					if note_output == 1 or note_output == 3 then
           	engine.hz(MusicUtil.note_num_to_freq(rhythmicDisplay[track]['n']))
 					elseif note_output == 2 or note_output == 3 then
-						midi_device[midi_target]:note_on(rhythmicDisplay[track]['n'])
+					  play_midi_note(rhythmicDisplay[track]['n'], noteEvents[i][3])
 					end
         end
       end
     end
-    clockPosition = util.round(clockPosition + tick, 0.0001)            -- move to next clock position
+    clockPosition = clockPosition + tick           -- move to next clock position
+    if (clockPosition >= 4 * clock_div) then clockPosition = 0 end  --loop clock
     clock.sync(1/192)                               -- and wait
   end
 end
 
 function init()
   redraw_clock_id = clock.run(redraw_clock) --add these for other clocks so we can kill them at the end
+  ticker_clock_id = clock.run(ticker)
+  --note_clock_id = clock.run(play_midi_note)
   clockPosition = 0
   screenDirty = true
   gridDirty = true
+  screen.line_width(1)
 
+  --playing notes stuff
 	note_destinations = {"audio", "midi", "audio + midi"}
 	note_output = 1
+  function play_midi_note(note, duration)  
+    midi_device[midi_target]:note_on(note)
+    local note_time = clock.get_beat_sec() * duration * 4 - 0.01
+    clock.run(
+      function()
+        clock.sleep(note_time)
+        midi_device[midi_target]:note_off(note, 0)
+      end
+    )
+  end
+
   --engine stuff
   engine.amp(1)
   engine.release(0.2)
@@ -102,6 +117,7 @@ function init()
   -- declare init cursor variables
   currentTrack,curXbeat,curXdiv,curXdisp,displayWidthBeat,curYPos=1,1,1,1,1,0
   tick = 1 / 192
+  clock_div = 1
   isPlaying = false
   -- calculate some other init cursor values
   -- 1 / number of beat * 1 / number of subdivs in current beat
@@ -117,6 +133,8 @@ function init()
   params:add_separator("-Scholastic Global-")
   params:add_number("tracksAmount", "Number of Tracks", 1, 8, 4)
   params:set_action("tracksAmount", function(x) tracksAmount = x end)
+  params:add_number("clock_div", "Clock Divide", 1, 4, 1)
+  params:set_action("clock_div", function(x) clock_div = x end)
 	params:add{type="option", id="note_output", name="Note Destination", options=note_destinations, default=1, action=function(x) note_output=x end}
 		--MIDI--
 	midi_device = {} -- container for connected midi devices
@@ -125,7 +143,7 @@ function init()
 
   for i = 1,#midi.vports do -- query all ports
     midi_device[i] = midi.connect(i) -- connect each device
-    table.insert(midi_device_names, "port"..i..": "..util.trim_string_to_width(midi_device[i].name,80) -- value to insert
+    table.insert(midi_device_names, i..": "..util.trim_string_to_width(midi_device[i].name,80) -- value to insert
     )
   end
   params:add_option("midi target", "MIDI Device",midi_device_names,1)
@@ -138,6 +156,7 @@ function init()
     action=function(x) engine.pw(x) end}
   --set notes for each track
   params:add_separator("Output Notes")
+  params:add_group("output notes", "output notes", 8)
   for i=1, 8 do
     rhythmicDisplay[i]['n'] = 36+i*2
     params:add_number("track"..i.."note", "Track "..i.." Note:", 1, 127, 36+i*2)
@@ -153,11 +172,14 @@ function init()
     tab.save(noteEvents,norns.state.data.."/"..number.."/notes.data")
   end
   params.action_read = function(filename,silent,number)
+    midi_device[midi_target]:cc(123,0,1) -- all notes off
     print("finished reading '"..filename.."'", number)
     note_data = tab.load(norns.state.data.."/"..number.."/display.data")
     rhythmicDisplay = note_data -- send this restored table to the sequins
     note_data = tab.load(norns.state.data.."/"..number.."/notes.data")
     noteEvents = note_data -- send this restored table to the sequins
+    updateCursor()
+    curYPos = math.floor((currentTrack - 1) * (screenHeight / tracksAmount))
   end
   params.action_delete = function(filename,name,number)
     print("finished deleting '"..filename, number)
@@ -185,14 +207,9 @@ end
 
 function redraw()
   screen.clear()
-  screen.line_width(1)
-  
-    -- rectangle for cursor background
---[[  screen.level(1)
-  screen.rect(curXdisp, curYPos, curXwidth, (screenHeight / tracksAmount))
-  screen.fill()--]]
-  --draw notes 2.0
-  screen.level(4)
+
+
+  screen.level(3)
   for i=1, #noteEvents do
     if noteEvents[i][3] then
       screen.rect(
@@ -204,7 +221,7 @@ function redraw()
     end
   end
 
-  --DON'T TOUCH -- THIS IS WORKING
+--DON'T TOUCH -- THIS IS WORKING
   -- lines for each beat and subdivision
   -- rectangles for notes
   -- for each track
@@ -219,35 +236,42 @@ function redraw()
         nowPixel = math.floor(nowPosition * screenWidth)
         nowHeight = math.floor(trackHeight * (i - 1) * screenHeight)
         --draw the playback
-        if isPlaying and clockPosition/4 >= nowPosition  and clockPosition/4 < nowPosition + displayWidthSubdiv then
+        if isPlaying and clockPosition/(4 * clock_div) >= nowPosition  and clockPosition/(4 * clock_div) < nowPosition + displayWidthSubdiv then
           --flash squares, or don't
-          local level = 1 + math.floor(10 * (nowPosition + displayWidthSubdiv - clockPosition /4))
+          local level = 1 + math.floor(10 * (nowPosition + displayWidthSubdiv - clockPosition / (4 * clock_div)))
           screen.level(level)
           screen.rect(nowPixel, nowHeight, math.floor(128 * displayWidthSubdiv), screenHeight / tracksAmount)
           screen.fill()
           for m=1, #noteEvents do
             if noteEvents[m][3] then
-              if i == noteEvents[m][1] and clockPosition/4 >= noteEvents[m][2] and clockPosition/4 < noteEvents[m][2] + noteEvents[m][3] then
-                local level = 8 + math.floor(25 * (nowPosition + displayWidthSubdiv - clockPosition/4))
+              if i == noteEvents[m][1] and clockPosition/(4 * clock_div) >= noteEvents[m][2] and clockPosition/(4*clock_div) < noteEvents[m][2] + noteEvents[m][3] then
+                local level = 8 + math.floor(25 * (nowPosition + displayWidthSubdiv - clockPosition/(4*clock_div)))
                 screen.level(level)
-                screen.rect(noteEvents[m][2] * 128, nowHeight, 128 * noteEvents[m][3], screenHeight / tracksAmount)
+                screen.rect(noteEvents[m][2] * 128, nowHeight, math.floor(screenWidth * noteEvents[m][3]), screenHeight / tracksAmount)
                 screen.fill()
               end
             end
           end
         end
         --draw the lines
-        screen.level(2)
         local gridlevel = 5
+        screen.level(6)
         if k == 1 then screen.level(15)
           gridlevel = 15 end
         screen.move(nowPixel, nowHeight)
-        screen.line_rel(1, screenHeight / tracksAmount)
+        screen.line_rel(0, screenHeight / tracksAmount)
         screen.stroke()
       end
     end
   end
   --DON"T TOUCH
+  
+  for m=1, #noteEvents do
+    screen.level(4)
+    screen.move(math.floor(noteEvents[m][2] * screenWidth), (noteEvents[m][1] - 1) * (screenHeight / tracksAmount))
+    screen.line_rel(0, screenHeight / tracksAmount)
+    screen.stroke()
+  end
   
   -- rectangle for cursor outside
   if currentTrack == 0 then
@@ -259,10 +283,10 @@ function redraw()
     screen.stroke()
   else
     curFlash()
-    screen.rect(curXdisp + 2, curYPos + 1, curXwidth - 1, (screenHeight / tracksAmount) - 1)
+    screen.rect(curXdisp+1, curYPos + 1, math.max(curXwidth - 2, 1), (screenHeight / tracksAmount) - 1)
     screen.stroke()
     screen.level(1)
-    screen.rect(curXdisp + 3, curYPos + 2, curXwidth - 3, (screenHeight / tracksAmount) - 3)
+    screen.rect(curXdisp+2, curYPos + 2, math.max(curXwidth - 4, 0), (screenHeight / tracksAmount) - 3)
     screen.stroke()
   end
   --HUD for values
@@ -301,25 +325,25 @@ function redraw()
       screen.move(64,34)
       screen.text_center(tracksAmount)
     else if hudTime < 0 then changedBeat = {}
-    else
-      if curXbeat > 0 then
-        local xpos = (changedBeat[2] - 1) * (screenWidth / rhythmicDisplay[currentTrack][1]) + ((screenWidth / rhythmicDisplay[currentTrack][1]) / 2)
-        screen.level(1)
-        screen.rect(xpos - 5,(currentTrack - 1) * screenHeight / tracksAmount + screenHeight / (tracksAmount * 2) -4,11,8)
-        screen.fill()
-        screen.level(15)
-        screen.move(xpos, 
-          changedBeat[1] * (screenHeight /tracksAmount) - ((screenHeight /tracksAmount) / 2) + 2)
-          screen.text_center(changedBeat[3])
-      else -- we changed beat
-        screen.level(1)
-        screen.rect(59,(currentTrack - 1) * screenHeight / tracksAmount + screenHeight / (tracksAmount * 2) -4,11,8)
-        screen.fill()
-        screen.level(15)
-        screen.move(64,(currentTrack - 1) * screenHeight / tracksAmount + screenHeight / (tracksAmount * 2) + 2)
-        screen.text_center(rhythmicDisplay[currentTrack][1])
+      else
+        if curXbeat > 0 then
+          local xpos = (changedBeat[2] - 1) * (screenWidth / rhythmicDisplay[currentTrack][1]) + ((screenWidth / rhythmicDisplay[currentTrack][1]) / 2)
+          screen.level(1)
+          screen.rect(xpos - 5,(currentTrack - 1) * screenHeight / tracksAmount + screenHeight / (tracksAmount * 2) -4,11,8)
+          screen.fill()
+          screen.level(15)
+          screen.move(xpos, 
+            changedBeat[1] * (screenHeight /tracksAmount) - ((screenHeight /tracksAmount) / 2) + 2)
+            screen.text_center(changedBeat[3])
+        else -- we changed beat
+          screen.level(1)
+          screen.rect(59,(currentTrack - 1) * screenHeight / tracksAmount + screenHeight / (tracksAmount * 2) -4,11,8)
+          screen.fill()
+          screen.level(15)
+          screen.move(64,(currentTrack - 1) * screenHeight / tracksAmount + screenHeight / (tracksAmount * 2) + 2)
+          screen.text_center(rhythmicDisplay[currentTrack][1])
+        end
       end
-    end
     end
   end
   
@@ -467,7 +491,7 @@ function enc(e, d)
     hudTime = 15
   end
 
-end
+end -- END OF ENCODERS
 
 function key(k, z)
   
@@ -502,22 +526,30 @@ function key(k, z)
   end
 
   if (k == 2 and z == 1) then
-    if isPlaying then
+    if heldKeys[1] then
+      if isPlaying then
+        isPlaying = false
+        midi_device[midi_target]:cc(123,0,1) -- all notes off
+      else
+        isPlaying = true end
+    else
+      if isPlaying then
       isPlaying = false
       clockPosition = 0
-      screenDirty = true
-      gridDirty= true
+      midi_device[midi_target]:cc(123,0,1) -- all notes off
     else
       isPlaying = true
       clock.run(ticker) -- need to call this every time? hmm
-      screenDirty = true
-      gridDirty= true
     end
-  end  
-end
+    end
+    screenDirty,gridDirty = true, true
+  end
+
+end -- end of buttons
 
 function cleanup() --------------- cleanup() is automatically called on script close
+  midi_device[midi_target]:cc(123,0,1) -- all notes off
   clock.cancel(redraw_clock_id) -- melt our clock via the id we noted
   -- should we melt the ticker clock too?
-  
+  clock.cancel(ticker_clock_id)
 end
